61d7acfa6627149b7b3e991e8a72f58f497579ea
Asma22334585 <guesmihadil0@gmail.com>
2024-04-22 13:08:41 -0700

    my commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,10 +8,22 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     id = argv[1]
 
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(complete)) for complete in completed]
+def get_todos_by_employee(employee_id):
+    """ Return list progress(todos)"""
+    api_url = 'https://jsonplaceholder.typicode.com/'
+    todos_url = f'{api_url}/users/{employee_id}/todos'
+    users_url = f'{api_url}/users/{employee_id}'
+
+    todos_list = requests.get(todos_url).json()
+    users_list = requests.get(users_url).json()
+
+    user_name = users_list['name']
+
+    completed = [task.get('title')
+                 for task in todos_list if task.get('completed') is True]
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(user_name, len(completed), len(todos_list)))
+
+    for task in completed:
+        print(f'\t {task}')
